189237ee203caa1f1ebf21d92499d42cdfe699e9
Tim Hoolihan <tim.hoolihan@dialogtech.com>
2018-03-07 10:32:25 -0500

    Refactor to combine similar methods

--- shared/plot_history.py
+++ shared/plot_history.py
@@ -1,32 +1,21 @@
 import matplotlib.pyplot as plt
 
-def plot_loss(history, fig=plt):
-    loss = history.history['loss']
-    val_loss = history.history['val_loss']
+def plot_metric(history, fig=plt, met_idx="loss", met_name="Loss"):
+    loss = history.history[met_idx]
+    val_loss = history.history['val_{}'.format(met_idx)]
     epochs = range(1, len(loss) + 1)
     fig.plot(epochs, loss, 'r-o', label='Training')
     fig.plot(epochs, val_loss, 'b-o', label='Validation')
     fig.set_xlabel('Epochs')
-    fig.set_ylabel('Loss')
-    fig.legend()
-    return fig
-
-def plot_accuracy(history, fig=plt):
-    loss = history.history['acc']
-    val_loss = history.history['val_acc']
-    epochs = range(1, len(loss) + 1)
-    fig.plot(epochs, loss, 'r-o', label='Training')
-    fig.plot(epochs, val_loss, 'b-o', label='Validation')
-    fig.set_xlabel('Epochs')
-    fig.set_ylabel('Accuracy')
+    fig.set_ylabel(met_name)
     fig.legend()
     return fig
 
 def plot_all(history):
     combined = plt.figure()
     ax1 = combined.add_subplot(2,1,1)
-    plot_loss(history, ax1)
+    plot_metric(history, ax1)
     ax2 = combined.add_subplot(2,1,2)
-    plot_accuracy(history, ax2)
+    plot_metric(history, ax2, 'acc', 'Accuracy')
     combined.show()
     return combined
