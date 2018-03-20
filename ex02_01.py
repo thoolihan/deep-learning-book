abf03aa6be06c07ebaf0f272144519725763083b
Tim Hoolihan <tim.hoolihan@dialogtech.com>
2018-03-20 14:59:23 -0400

    Added F1

--- ex02_01.py
+++ ex02_01.py
@@ -2,9 +2,13 @@ from keras.datasets import mnist
 from keras import models
 from keras import layers
 from keras.utils import to_categorical
-from shared.logger import get_logger
+from shared.logger import get_logger, get_filename, get_start_time, get_curr_time
 from shared.transform import flatten, scale
+from shared.metrics import f1_score
+from shared.plot_history import plot_all
+from shared.utility import open_plot
 
+OUTPUT_DIR="output/ex02"
 logger = get_logger()
 
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
@@ -23,11 +27,20 @@ model.add(layers.Dense(512, activation='relu', input_shape=(28 * 28,)))
 model.add(layers.Dense(10, activation='softmax'))
 model.compile(optimizer = 'rmsprop',
                loss = 'categorical_crossentropy',
-               metrics = ['accuracy'])
+               metrics = ['accuracy', f1_score])
 
 # fit
-model.fit(x=train_images, y=train_labels, epochs=5, batch_size=128)
+history = model.fit(x=train_images, y=train_labels, epochs=5, batch_size=128)
 
 # test
-test_loss, test_acc = model.evaluate(x=test_images, y=test_labels)
-logger.info("Test loss: {} Test accuracy: {}".format(test_loss, test_acc))
+test_loss, test_acc, test_f1 = model.evaluate(x=test_images, y=test_labels)
+logger.info("Test loss: {} Test accuracy: {} Test F1: {}".format(test_loss, test_acc, test_f1))
+
+logger.info("saving plot of loss and accuracy")
+plots = plot_all(history, {'acc': 'Accuracy', 'f1_score': 'F-score'})
+pname = "{}/{}-{}.png".format(OUTPUT_DIR, get_filename(), get_start_time())
+plots.savefig(pname)
+logger.info("created {}".format(pname))
+logger.info("finished at {}".format(get_curr_time()))
+
+open_plot(pname)
--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -6,13 +6,24 @@ from sklearn.metrics import f1_score as sk_f1_score
 
 class TestContinuous(unittest.TestCase):
 
-    def test_fl_to_zero_ten(self):
+    def test_f1_score(self):
         y_true = np.array([0., 0., 1., 1.], dtype=K.floatx())
         y_pred = np.array([0., 1., 1., 0.], dtype=K.floatx())
         y_true_k = K.variable(y_true)
         y_pred_k = K.variable(y_pred)
 
         expected = sk_f1_score(y_true, y_pred)
+        actual = K.eval(f1_score(y_true_k, y_pred_k))
+
+        self.assertEqual(expected, actual)
+
+    def test_mc_f1_score(self):
+        y_true = np.array([[0., 1., 0.], [1., 0., 0.], [0., 0., 1.]], dtype=K.floatx())
+        y_pred = np.array([[1., 0., 0.], [0., 1., 0.], [0., 0., 1.]], dtype=K.floatx())
+        y_true_k = K.variable(y_true)
+        y_pred_k = K.variable(y_pred)
+
+        expected = sk_f1_score(y_true, y_pred, average='micro')
         actual = K.eval(f1_score(y_true_k, y_pred_k))
 
         self.assertEqual(expected, actual)
