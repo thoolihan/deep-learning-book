a36eaa86b0bbed70571541698521f6687880179e
Tim Hoolihan <tim.hoolihan@dialogtech.com>
2018-04-07 11:44:29 -0400

    Crossval version created, with Essay Counting

--- k02mvp.py
+++ k02mvp.py
@@ -3,6 +3,7 @@ import os
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 from shared.logger import get_logger, get_start_time, get_filename, get_curr_time
+from shared.donors.transform import count_essays, ESSAY_COUNT
 
 # Constants and Config for index, features, and label
 PROJECT_NAME="donors"
@@ -12,7 +13,7 @@ INDEX_COL="id"
 LABEL = 'project_is_approved'
 CREATE_OUTPUT = True
 FEATURES = ['teacher_number_of_previously_posted_projects']
-COLS = [INDEX_COL] + FEATURES
+COLS = [INDEX_COL] + FEATURES + ['project_essay_1', 'project_essay_2', 'project_essay_3', 'project_essay_4']
 COLS_TR = COLS + [LABEL]
 DTYPES = {
     'teacher_number_of_previously_posted_projects': 'int64'
@@ -28,6 +29,11 @@ logger.info("loading data")
 train_df = pd.read_csv("{}/train.zip".format(INPUT_DIR), usecols=COLS_TR, index_col = INDEX_COL, dtype=DTYPES)
 test_df = pd.read_csv("{}/test.zip".format(INPUT_DIR), usecols=COLS, index_col = INDEX_COL, dtype=DTYPES_TR)
 train_df = train_df.sample(frac=1)
+
+# count essays
+count_essays(train_df)
+count_essays(test_df)
+FEATURES += [ESSAY_COUNT]
 
 # model
 model = LogisticRegression()
---
+++ shared/donors/transform.py
@@ -0,0 +1,20 @@
+import pandas as pd
+ESSAY_COUNT = 'essay_count'
+
+def col_name(index):
+    return "{}_{}".format(ESSAY_COUNT, index)
+
+def has_content(essay):
+    if pd.isnull(essay) or essay == "":
+        return 0
+    return 1
+
+def add_essays(row):
+    return row[col_name(1)] + row[col_name(2)] + row[col_name(3)] + row[col_name(4)]
+
+def count_essays(df):
+    df[ESSAY_COUNT] = df.project_essay_1.map(has_content)
+    df[ESSAY_COUNT] = df[ESSAY_COUNT].add(df.project_essay_2.map(has_content))
+    df[ESSAY_COUNT] = df[ESSAY_COUNT].add(df.project_essay_3.map(has_content))
+    df[ESSAY_COUNT] = df[ESSAY_COUNT].add(df.project_essay_4.map(has_content))
+    return df
