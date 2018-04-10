0716122163d39b04aee6469864a5ee381e06bb81
Tim Hoolihan <tim.hoolihan@dialogtech.com>
2018-04-10 19:33:59 -0400

    Cache numpy matrices

--- k02mvp3.py
+++ k02mvp3.py
@@ -7,6 +7,8 @@ from sklearn.model_selection import cross_val_score
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder, StandardScaler
 from shared.logger import get_logger, get_start_time, get_filename, get_curr_time
 from shared.donors.transform import count_essays, ESSAY_COUNT
+from hashlib import md5
+from pathlib import Path
 
 # Constants and Config for index, features, and label
 PROJECT_NAME="donors"
@@ -16,22 +18,22 @@ INDEX_COL="id"
 LABEL = 'project_is_approved'
 CREATE_OUTPUT = True
 FEATURES = ['teacher_number_of_previously_posted_projects']
-OHE_FEATURES = ['school_state', 'project_subject_categories', 'teacher_id', 'teacher_prefix', 'project_grade_category']
+OHE_FEATURES = ['school_state', 'project_subject_categories', 'teacher_prefix', 'project_grade_category']
 ESSAY_COLS =  ['project_essay_1', 'project_essay_2', 'project_essay_3', 'project_essay_4']
 COLS = [INDEX_COL] + FEATURES + OHE_FEATURES + ESSAY_COLS
 COLS_TR = COLS + [LABEL]
 DTYPES = {
-    'id': str,
+    'id': object,
     'teacher_number_of_previously_posted_projects': int,
-    'school_state': str,
-    'project_sub_categories': str,
-    'teacher_id': str,
-    'teacher_prefix': str,
-    'project_grade_category': str,
-    'project_essay_1': str,
-    'project_essay_2': str,
-    'project_essay_3': str,
-    'project_essay_4': str
+    'school_state': object,
+    'project_sub_categories': object,
+    'teacher_id': object,
+    'teacher_prefix': object,
+    'project_grade_category': object,
+    'project_essay_1': object,
+    'project_essay_2': object,
+    'project_essay_3': object,
+    'project_essay_4': object
 }
 DTYPES_TR = DTYPES.copy()
 DTYPES_TR[LABEL] = np.float32
@@ -41,36 +43,51 @@ logger.info("running on {}".format(os.name))
 
 # load data
 logger.info("loading data")
-train_df = pd.read_csv("{}/train.zip".format(INPUT_DIR), usecols=COLS_TR, index_col = INDEX_COL, dtype=DTYPES)
+
+# test_df has to be read regardless since it's used to create submission file
+logger.info("reading test file")
 test_df = pd.read_csv("{}/test.zip".format(INPUT_DIR), usecols=COLS, index_col = INDEX_COL, dtype=DTYPES_TR)
-train_df = train_df.sample(frac=1)
 
-# create features
-train_df = count_essays(train_df)
-train_df = train_df.drop(columns = ESSAY_COLS)
-test_df = count_essays(test_df)
-test_df = test_df.drop(columns = ESSAY_COLS)
+fields = ":".join(COLS)
+hash = md5(fields.encode('utf-8')).hexdigest()
+logger.debug("columns hash is {}".format(hash))
+cached_numpy_file = Path("{}/cached/{}.npz".format(INPUT_DIR, hash))
 
-# Encode features
-#  dict of encoders for later saving if need be
-lencoders = {}
-for col in OHE_FEATURES:
-    # String to int
-    lencoders[col] = LabelEncoder()
-    lencoders[col].fit((train_df[col]).append(test_df[col]))
-    train_df[col] = lencoders[col].transform(train_df[col])
-    test_df[col] = lencoders[col].transform(test_df[col])
+if cached_numpy_file.exists():
+    logger.info("reading cached files from numpy zip")
 
-# to matrix
-X_train = train_df.drop(columns = [LABEL]).as_matrix()
-y_train = train_df[LABEL]
-X_test = test_df.as_matrix()
+    with cached_numpy_file.open('rb') as fh:
+        cached_info = np.load(fh)
+        X_train = cached_info['X_train']
+        y_train = cached_info['y_train']
+        X_test = cached_info['X_test']
+else:
+    logger.info("reading original train file")
+    train_df = pd.read_csv("{}/train.zip".format(INPUT_DIR), usecols=COLS_TR, index_col = INDEX_COL, dtype=DTYPES)
+    train_df = train_df.sample(frac=1)
 
-# Categorical ints to bit field
-col_mask = [col in OHE_FEATURES for col in train_df.drop(columns = [LABEL]).columns.values]
-ohe = OneHotEncoder(sparse=False, categorical_features=col_mask, handle_unknown='ignore')
-X_train = ohe.fit_transform(X_train)
-X_test = ohe.fit_transform(X_test)
+    # create features
+    logger.info("counting essays")
+    train_df = count_essays(train_df)
+    train_df = train_df.drop(columns = ESSAY_COLS)
+    test_df = count_essays(test_df)
+    test_df = test_df.drop(columns = ESSAY_COLS)
+
+    # Encode features
+    logger.info("encoding dummy variables")
+    train_df = pd.get_dummies(train_df, columns = OHE_FEATURES)
+    test_df = pd.get_dummies(test_df, columns = OHE_FEATURES)
+    test_df.reindex(columns = train_df.drop(columns = [LABEL]).columns.values, fill_value=0)
+
+    # to matrix
+    logger.info("dataframes to matrices")
+    X_train = train_df.drop(columns = [LABEL]).as_matrix()
+    y_train = train_df[LABEL].values
+    X_test = test_df.as_matrix()
+
+    logger.info("caching numpy files")
+    with cached_numpy_file.open('wb') as fh:
+        np.savez(fh, X_train = X_train, y_train =  y_train, X_test = X_test)
 
 # model
 model = LogisticRegression()
@@ -79,7 +96,7 @@ logger.debug("Training data has shapes of X: {} y: {}".format(X_train.shape, y_t
 logger.debug("Test data has shapes of X: {}".format(X_test.shape))
 
 # evaluate model
-scores = cross_val_score(model, X=X_train, y=y_train, cv=10, scoring='roc_auc')
+scores = cross_val_score(model, X=X_train, y=y_train, cv=5, scoring='roc_auc')
 logger.info("ROC: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 
 # fit all
