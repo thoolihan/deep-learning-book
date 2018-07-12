05048c3ffa94033541adac303732c72c8da058d4
Tim Hoolihan <tim.hoolihan@dialogtech.com>
2018-07-12 17:40:44 -0400

    Convolutional AutoEncoder added

--- shared/transform.py
+++ shared/transform.py
@@ -1,6 +1,11 @@
+import numpy as np
+
 def flatten(data):
     i, h, w = data.shape
     return data.reshape(i, h * w)
 
 def scale(data):
     return data.astype('float32') / 255.
+
+def add_channel(data):
+    return np.expand_dims(data, -1)
