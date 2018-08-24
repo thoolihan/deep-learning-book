d8889cbd5cd0a7e4d727be5f48085db3c8001b3d
Tim Hoolihan <tim.hoolihan@dialogtech.com>
2018-08-24 14:42:25 -0400

    Setting agg for matplotlib before import pyplot

--- shared/plot_history.py
+++ shared/plot_history.py
@@ -1,7 +1,4 @@
 import matplotlib
-import matplotlib.pyplot as plt
-import os
-plt.style.use('ggplot')
 
 def has_display():
     if os.name == 'posix' and "DISPLAY" in os.environ:
@@ -11,6 +8,10 @@ def has_display():
 
 if not has_display():
     matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt
+import os
+plt.style.use('ggplot')
 
 def plot_metric(history, ax, met_idx="loss", met_name="Loss"):
     loss = history.history[met_idx]
