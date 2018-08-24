72f66429a193409469c9b5c3501a0f705095551b
Tim Hoolihan <tim.hoolihan@dialogtech.com>
2018-08-24 15:38:25 -0400

    Ensure output directory in ch 5 example

--- ex05_01.py
+++ ex05_01.py
@@ -6,19 +6,20 @@ from shared.logger import get_logger, get_filename, get_start_time, get_curr_tim
 from shared.transform import flatten, scale
 from shared.metrics import f1_score
 from shared.plot_history import plot_all
-from shared.utility import open_plot
+from shared.utility import open_plot, ensure_directory
+
+logger = get_logger()
 
 # Constants and Config for index, features, and label
 PROJECT_NAME="ex05"
 INPUT_DIR="data/{}".format(PROJECT_NAME)
 OUTPUT_DIR="output/{}".format(PROJECT_NAME)
+ensure_directory(OUTPUT_DIR, logger)
 DRO = 0.25
 L2R = 0.0001
 HLAYER = 64
 EPOCHS = 5
 BATCH_SIZE = 64
-
-logger = get_logger()
 
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
