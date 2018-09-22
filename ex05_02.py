081ea5d067ebd62ec3a26e079ee57529528200bb
Tim Hoolihan <tim@hoolihan.net>
2018-09-21 21:41:14 -0400

    Working on cnn for cats and dogs that will be driven by generator

--- ex05_02.py
+++ ex05_02.py
@@ -1,7 +1,5 @@
-from keras.datasets import mnist
-from keras import models
-from keras import layers
-from keras.utils import to_categorical
+from keras import models, layers, optimizers
+from keras.preprocessing.image import ImageDataGenerator
 from shared.logger import get_logger, get_filename, get_start_time, get_curr_time
 from shared.transform import flatten, scale
 from shared.metrics import f1_score
@@ -17,9 +15,34 @@ OUTPUT_DIR="output/{}".format(PROJECT_NAME)
 ensure_directory(INPUT_DIR, logger)
 ensure_directory(OUTPUT_DIR, logger)
 DRO = 0.25
-L2R = 0.0001
+LR = 1e-4
 HLAYER = 64
 EPOCHS = 5
 BATCH_SIZE = 64
+img_shape = (150, 150, 3)
 
 # load data
+train_datagen = ImageDataGenerator(rescale=1./255)
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_dir,
+)
+
+# build model
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=img_shape))
+model.add(layers.MaxPooling2D(2, 2))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D(2, 2))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D(2, 2))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D(2, 2))
+model.add(layers.Flatten())
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer=optimizers.RMSprop(lr=LR),
+              metrics=['acc', f1_score])
