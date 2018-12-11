1abeee906ff4272d34d5adbfcc8a984a544db670
Tim Hoolihan <tim.hoolihan@dialogtech.com>
2018-12-11 10:39:37 -0500

    Added a version of ex03 that uses keras implementation embedded in tensorflow package See https://medium.com/tensorflow/standardizing-on-keras-guidance-on-high-level-apis-in-tensorflow-2-0-bad2b04c819a

---
+++ ex03_01tf.py
@@ -0,0 +1,46 @@
+from tensorflow.keras.datasets import imdb
+from tensorflow.keras import models
+from tensorflow.keras import layers
+from tensorflow.keras import optimizers
+from tensorflow.keras import losses
+from tensorflow.keras import metrics
+import numpy as np
+from shared.logger import get_logger
+
+logger = get_logger()
+EPOCHS = 5
+BATCH_SIZE = 256
+NUMWORDS = 5000
+VALSIZE = 10000
+def vectorize_sequence(sequences, dimension=NUMWORDS):
+    results = np.zeros((len(sequences), dimension))
+    for i, sequence in enumerate(sequences):
+        results[i, sequence] = 1.
+    return results
+
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=NUMWORDS)
+
+logger.info("Vectorizing data")
+x_train = vectorize_sequence(train_data)
+x_test = vectorize_sequence(test_data)
+
+y_train = np.asarray(train_labels, dtype='float32')
+y_test = np.asarray(test_labels, dtype='float32')
+
+model = models.Sequential()
+model.add(layers.Dense(16, activation='relu', input_shape=(NUMWORDS,)))
+model.add(layers.Dense(16, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+logger.debug("Compiling Model")
+model.compile(optimizer=optimizers.RMSprop(lr=.001),
+              loss=losses.binary_crossentropy,
+              metrics=[metrics.binary_accuracy])
+
+logger.info("Fitting Model")
+history = model.fit(x_train,
+          y_train,
+          epochs=EPOCHS,
+          batch_size=BATCH_SIZE,
+          validation_split=0.3,
+          shuffle=True)
