ee52a740e1c94398dd96b4c0812aaf749a822116
Tim Hoolihan <tim@hoolihan.net>
2019-03-27 18:07:11 -0400

    Run a prediction

--- ex05_12.py
+++ ex05_12.py
@@ -1,9 +1,9 @@
 import os
 import numpy as np
-from functools import reduce
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing import image
 from keras.applications.imagenet_utils import preprocess_input
 from tensorflow.keras.applications import VGG16
+from tensorflow.keras.applications.vgg16 import preprocess_input, decode_predictions
 from tensorflow.keras import models, layers, optimizers
 from tensorflow.keras.callbacks import TensorBoard
 from shared.logger import get_logger, get_filename, get_start_time, get_curr_time
@@ -15,7 +15,7 @@ logger = get_logger()
 model = VGG16(weights='imagenet')
 
 # Constants and Config for index, features, and label
-PROJECT_NAME="vgg_elephant"
+PROJECT_NAME="lion"
 INPUT_DIR="data/{}".format(PROJECT_NAME)
 OUTPUT_DIR="output/{}".format(PROJECT_NAME)
 ensure_directory(OUTPUT_DIR, logger)
@@ -23,3 +23,13 @@ MODEL_FILE = get_model_file(OUTPUT_DIR)
 TBLOGDIR=get_tensorboard_directory(PROJECT_NAME)
 logger.info("Tensorboard is at: {}".format(TBLOGDIR))
 
+img_path = '{}/white_lion.jpg'.format(INPUT_DIR)
+img = image.load_img(img_path, target_size=(224, 224))
+
+x = image.img_to_array(img)
+
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+
+preds = model.predict(x)
+print('Predicted:', decode_predictions(preds, top=3)[0])
