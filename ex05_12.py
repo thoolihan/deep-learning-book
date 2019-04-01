6c653ac5b4f82979f686c5e24a773eed513bb54e
Tim Hoolihan <tim@hoolihan.net>
2019-04-01 16:51:15 -0400

    Get heatmap of image detection

--- ex05_12.py
+++ ex05_12.py
@@ -5,17 +5,19 @@ from keras.applications.imagenet_utils import preprocess_input
 from tensorflow.keras.applications import VGG16
 from tensorflow.keras.applications.vgg16 import preprocess_input, decode_predictions
 from tensorflow.keras import models, layers, optimizers
+from tensorflow.keras import backend as K
 from tensorflow.keras.callbacks import TensorBoard
 from shared.logger import get_logger, get_filename, get_start_time, get_curr_time
 from shared.metrics import f1_score
 from shared.utility import open_plot, ensure_directory, get_tensorboard_directory, get_model_file
+import matplotlib
 
 logger = get_logger()
 
 model = VGG16(weights='imagenet')
 
 # Constants and Config for index, features, and label
-PROJECT_NAME="lion"
+PROJECT_NAME="animals"
 INPUT_DIR="data/{}".format(PROJECT_NAME)
 OUTPUT_DIR="output/{}".format(PROJECT_NAME)
 ensure_directory(OUTPUT_DIR, logger)
@@ -23,7 +25,8 @@ MODEL_FILE = get_model_file(OUTPUT_DIR)
 TBLOGDIR=get_tensorboard_directory(PROJECT_NAME)
 logger.info("Tensorboard is at: {}".format(TBLOGDIR))
 
-img_path = '{}/white_lion.jpg'.format(INPUT_DIR)
+animal_img = "elephants"
+img_path = '{}/{}.jpg'.format(INPUT_DIR, animal_img)
 img = image.load_img(img_path, target_size=(224, 224))
 
 x = image.img_to_array(img)
@@ -32,4 +35,25 @@ x = np.expand_dims(x, axis=0)
 x = preprocess_input(x)
 
 preds = model.predict(x)
-print('Predicted:', decode_predictions(preds, top=3)[0])
+logger.info('Predicted: {}'.format(decode_predictions(preds, top=3)[0]))
+
+african_elephant_output = model.output[:, 386]
+last_conv_layer = model.get_layer('block5_conv3')
+grads = K.gradients(african_elephant_output, last_conv_layer.output)[0]
+
+pooled_grads = K.mean(grads, axis=(0, 1, 2))
+
+iterate = K.function([model.input], 
+                    [pooled_grads, last_conv_layer.output[0]])
+
+pooled_grads_value, conv_layer_output_value = iterate([x])
+
+for i in range(512):
+    conv_layer_output_value[:, :, i] *= pooled_grads_value[i]
+    
+heatmap = np.mean(conv_layer_output_value, axis=-1)
+heatmap = np.maximum(heatmap, 0)
+heatmap /= np.max(heatmap)
+heatmap_file = "{}/{}-heatmap.jpg".format(OUTPUT_DIR, animal_img)
+matplotlib.image.imsave(heatmap_file, heatmap)
+logger.info("Wrote {}".format(heatmap_file))
