305d24e65333356d18bd21fbe4a7e649d5e9fb18
Tim Hoolihan <tim@hoolihan.net>
2019-05-21 10:01:50 -0400

    Quality of life updates for old example, including sklearn evals

--- ex02_01.py
+++ ex02_01.py
@@ -1,14 +1,19 @@
-from keras.datasets import mnist
-from keras import models
-from keras import layers
-from keras.utils import to_categorical
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras import models
+from tensorflow.keras import layers
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.callbacks import TensorBoard
+from sklearn import metrics
+import numpy as np
 from shared.logger import get_logger, get_filename, get_start_time, get_curr_time
 from shared.transform import flatten, scale
 from shared.metrics import f1_score
 from shared.plot_history import plot_all
-from shared.utility import open_plot, ensure_directory
+from shared.utility import open_plot, ensure_directory, get_tensorboard_directory, get_model_file
 
+PROJECT_NAME="mnist"
 OUTPUT_DIR="output/ex02"
+TBLOGDIR=get_tensorboard_directory(PROJECT_NAME)
 logger = get_logger()
 ensure_directory(OUTPUT_DIR, logger)
 
@@ -31,17 +36,20 @@ model.compile(optimizer = 'rmsprop',
                metrics = ['accuracy', f1_score])
 
 # fit
-history = model.fit(x=train_images, y=train_labels, epochs=5, batch_size=128)
+history = model.fit(x=train_images, 
+                    y=train_labels, 
+                    epochs=5, 
+                    batch_size=128,
+                    callbacks=[TensorBoard(log_dir=TBLOGDIR)])
 
 # test
+logger.info("model evaluate")
 test_loss, test_acc, test_f1 = model.evaluate(x=test_images, y=test_labels)
 logger.info("Test loss: {} Test accuracy: {} Test F1: {}".format(test_loss, test_acc, test_f1))
 
-logger.info("saving plot of loss and accuracy")
-plots = plot_all(history, {'acc': 'Accuracy', 'f1_score': 'F-score'})
-pname = "{}/{}-{}.png".format(OUTPUT_DIR, get_filename(), get_start_time())
-plots.savefig(pname)
-logger.info("created {}".format(pname))
-logger.info("finished at {}".format(get_curr_time()))
+logger.info("sklearn evaluate")
+y_test_pred = np.round(model.predict(test_images))
+logger.info("sklearn f1 scores: {}".format(metrics.f1_score(test_labels, y_test_pred, average=None)))
+logger.info("classification report:\n{}".format(metrics.classification_report(test_labels, y_test_pred)))
 
-open_plot(pname)
+logger.info("finished at {}".format(get_curr_time()))
